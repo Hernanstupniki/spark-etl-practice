0f4a9304905d7a3773d9461ab82e252cb972dc98
Hernanstupniki <hernanst7@gmail.com>
2026-01-03 03:14:26 -0300

    Extend gold layer with dispersion metrics, percentiles and duration segmentation.

--- src/test_gold_one_table.py
+++ src/test_gold_one_table.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import count, avg, min, max, row_number, desc, when, col
+from pyspark.sql.functions import count, avg, min, max, row_number, desc, when, col, stddev, expr
 from pyspark.sql.window import Window
 
 spark = SparkSession.builder \
@@ -34,7 +34,6 @@ df_gold_by_lenght = df_silver_clean.groupBy("length").agg(
     avg("rental_rate").alias("avg_rental_rate"),
 )
 
-
 # Most frequent rating per length
 df_length_rating_count = (
     df_silver_clean
@@ -64,6 +63,37 @@ df_mode_reliability = (
     )
 
 
-df_mode_reliability.show(200, truncate = False)
+# standar deviation
+df_standard_deviation = df_silver_clean.agg(
+    stddev("length").alias("stddev_length"),
+    stddev("rental_rate").alias("stddev_rental_rate"),
+)
+
+# Length percentiles
+df_gold_length_percentiles = df_silver_clean.agg(
+    expr("percentile_approx(length, 0.25)").alias("p25_length"),
+    expr("percentile_approx(length, 0.50)").alias("median_length"),
+    expr("percentile_approx(length, 0.75)").alias("p75_length")
+)
+
+# rental rate percentiles
+df_gold_rental_rate_percentiles = df_silver_clean.agg(
+    expr("percentile_approx(rental_rate, 0.25)").alias("p25_rental_rate"),
+    expr("percentile_approx(rental_rate, 0.50)").alias("median_rental_rate"),
+    expr("percentile_approx(rental_rate, 0.75)").alias("p75_rental_rate")
+)
+
+# Length segmentation
+df_length_segmentation = (
+    df_silver_clean
+    .withColumn(
+    "length_segment",
+    when(col("length") < 50, "short")
+    .when((col("length") >= 50) & (col("length") < 100), "medium")
+    .otherwise("long"))
+    )
+
+
+df_length_segmentation.select("film_id", "title", "length_segment").show(200, truncate = False)
 
 df_gold_metrics.write.mode("overwrite").parquet("data/gold/film_metrics")
